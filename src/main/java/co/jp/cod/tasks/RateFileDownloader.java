31a17c3528223ad49cd6cb3e7eb4f4c2e9bc6d3a
shibadog <mischievous.dog00@gmail.com>
2018-07-09 11:50:15 +0900

    レートのダウンロードURLが変わったので修正/過去日付の復旧用APIを追加/mvnwのjarをバージョン管理に含める

--- src/main/java/co/jp/cod/tasks/RateFileDownloader.java
+++ src/main/java/co/jp/cod/tasks/RateFileDownloader.java
@@ -1,7 +1,20 @@
 package co.jp.cod.tasks;
 
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
+
 import org.apache.logging.log4j.util.Strings;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.web.client.RestTemplateBuilder;
@@ -9,18 +22,6 @@ import org.springframework.http.converter.StringHttpMessageConverter;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestTemplate;
-
-import java.io.BufferedWriter;
-import java.io.IOException;
-import java.nio.charset.Charset;
-import java.nio.charset.StandardCharsets;
-import java.nio.file.Files;
-import java.nio.file.Paths;
-import java.time.LocalDateTime;
-import java.time.format.DateTimeFormatter;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
 
 @Component
 public class RateFileDownloader {
@@ -33,9 +34,12 @@ public class RateFileDownloader {
 
     @Scheduled(cron = "0 0 13 * * 1-5", zone = "Asia/Tokyo")
     public void download() throws JsonProcessingException {
+        downloadFor(LocalDate.now());
+    }
 
-        String targetDate = LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyyMMdd"));
-        String accessTargetUrl = targetUrl.replace("yyyyMMdd", targetDate);
+    public void downloadFor(LocalDate targetDate) throws JsonProcessingException {
+        String targetDateFormat = targetDate.format(DateTimeFormatter.ofPattern("uuuuMMdd"));
+        String accessTargetUrl = targetUrl.replace("yyyyMMdd", targetDateFormat);
 
         RestTemplate template = new RestTemplateBuilder().build();
         template.getMessageConverters().add(0, new StringHttpMessageConverter(Charset.forName("Shift_JIS")));
@@ -58,10 +62,9 @@ public class RateFileDownloader {
         Rates rates = new Rates();
         rates.setRates(rateList);
 
-        // Json変換
         String json = new ObjectMapper().writeValueAsString(rates);
 
-        try (BufferedWriter br = Files.newBufferedWriter(Paths.get(saveDirectory + "quote_" + targetDate + ".json"), StandardCharsets.UTF_8)) {
+        try (BufferedWriter br = Files.newBufferedWriter(Paths.get(saveDirectory + "quote_" + targetDateFormat + ".json"), StandardCharsets.UTF_8)) {
             br.write(json);
             br.flush();
         } catch (IOException e) {
